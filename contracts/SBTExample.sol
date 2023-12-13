32156bb0457d3317cbc2f7aac1f8104b9f10fe51
ikmz <ikmz0104.official@gmail.com>
2023-12-14 01:27:05 +0900

    Add _beforeTokenTransfer function to SBTExample contract

---
+++ contracts/SBTExample.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// https://zenn.dev/ryo_takahashi/articles/d4bdc137b564db
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract SBTExample is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenCounter;
+
+    constructor() ERC721("SBTExample", "NEKO") {}
+
+    function mint() public {
+        _tokenCounter.increment();
+
+        uint256 newItemId = _tokenCounter.current();
+        // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol#L280-L292
+        _mint(msg.sender, newItemId);
+    }
+
+    // ここを追加するだけ
+    // _beforeTokenTransfer なくね？
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal pure override {
+        // mintは許可（そのまま処理を通す）
+      	// transferは禁止（処理を中断させる）
+        require(from == address(0));
+    }
+}
