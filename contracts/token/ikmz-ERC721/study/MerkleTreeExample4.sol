9af79b24d0981b4ae2627e1f6e09088544bb8dc3
ikmz <ikmz0104.official@gmail.com>
2023-12-14 19:37:52 +0900

    Add setMerkleRoot function to MyERC721

--- contracts/token/ikmz-ERC721/study/MerkleTreeExample4.sol
+++ contracts/token/ikmz-ERC721/study/MerkleTreeExample4.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 // code: https://zenn.dev/microverse_dev/articles/how-to-allowlist-mint
 // knowledge: https://zenn.dev/retocrooman/articles/98badb1cf200d9
 
-contract MyERC721 is ERC721URIStorage {
+contract MyERC721 is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    bytes32 public merkleRoot;
 
     constructor() ERC721("MyERC721", "MY721") {}
 
@@ -18,5 +21,9 @@ contract MyERC721 is ERC721URIStorage {
         uint256 newTokenId = _tokenIds.current();
         _mint(msg.sender, newTokenId);
         return newTokenId;
+    }
+
+    function setMerkleRoot(bytes32 _merkleRoot) public onlyOwner {
+        merkleRoot = _merkleRoot;
     }
 }
