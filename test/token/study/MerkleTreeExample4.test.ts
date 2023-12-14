dc78500c96c3197e3ab5f83dd33fa9e247806b68
ikmz <ikmz0104.official@gmail.com>
2023-12-14 19:36:32 +0900

    Remove unused code and imports

--- test/token/study/MerkleTreeExample4.test.ts
+++ test/token/study/MerkleTreeExample4.test.ts
@@ -1,6 +1,3 @@
-// https://medium.com/@ItsCuzzo/using-merkle-trees-for-nft-whitelists-523b58ada3f9
-//
-// 1. Import libraries. Use `npm` package manager to install
 const { MerkleTree } = require('merkletreejs');
 const keccak256 = require('keccak256');
 const { expect } = require("chai");
@@ -10,9 +7,6 @@ import chai from "chai";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import ChaiAsPromised from "chai-as-promised";
 
-// Typically, Chai is used for synchronous tests, 
-// but when dealing with asynchronous operations such as testing Ethereum smart contracts,
-// the ChaiAsPromised plugin comes in handy.
 chai.use(ChaiAsPromised);
 
 describe("MyERC721", async function () {
