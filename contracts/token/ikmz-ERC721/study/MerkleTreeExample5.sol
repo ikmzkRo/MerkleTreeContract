2428c7a1a906333e409aa081376b972764267bfe
ikmz <ikmz0104.official@gmail.com>
2023-12-15 12:33:54 +0900

    Add whitelistMint function to mint NFTs for whitelisted addresses

--- contracts/token/ikmz-ERC721/study/MerkleTreeExample5.sol
+++ contracts/token/ikmz-ERC721/study/MerkleTreeExample5.sol
@@ -1,37 +1,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import {MerkleProof} from '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
 
 // ホワイトリストの各アドレスに対して発行できる個数を制御できるようにする
 // ref: https://dev.to/peterblockman/understand-merkle-tree-by-making-a-nft-minting-whitelist-1148#step-4--verify-users--address-and-quantity
 
-contract IkmzERC721WLAQ is ERC721URIStorage, Ownable {
+contract IkmzERC721WLAQ is ERC721, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+
     bytes32 public merkleRoot;
+
     mapping(address => bool) public whitelistClaimed;
 
-    constructor() ERC721("whitelist", "WL") {}
+    // デプロイ時に計算結果のマークルルートをデプロイする
+    constructor(bytes32 merkleRoot_) ERC721('zuyomayo', 'ZTMY') {
+        merkleRoot = merkleRoot_;
+    }
 
-    function whitelistMint(bytes32[] calldata _merkleProof) public payable returns (uint256) {
+    function whitelistMint(uint256 quantity, bytes32[] calldata merkleProof) public {
         require(!whitelistClaimed[msg.sender], "Address already claimed");
-        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+
+        bytes32 node = keccak256(abi.encodePacked(msg.sender));
         require(
-            MerkleProof.verify(_merkleProof, merkleRoot, leaf),
+            MerkleProof.verify(merkleProof, merkleRoot, node),
             "Invalid proof"
         );
-        
-        _tokenIds.increment();
-        uint256 newTokenId = _tokenIds.current();
-        _mint(msg.sender, newTokenId);
+
+        for (uint256 i = 0; i < quantity; i++) {
+            uint256 tokenId = _tokenIds.current();
+            _mint(msg.sender, tokenId);
+
+            _tokenIds.increment();   
+        }
 
         whitelistClaimed[msg.sender] = true;
-        
-        return newTokenId;
     }
 
     function setMerkleRoot(bytes32 _merkleRoot) public onlyOwner {
