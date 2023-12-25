dbe318c1469f90ac823107260e56b14febcc8038
ikmz <ikmz0104.official@gmail.com>
2023-12-25 20:39:09 +0900

    Add external link to contract documentation

--- contracts/mock/UpgradeableContractExample.sol
+++ contracts/mock/UpgradeableContractExample.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+// https://qiita.com/biga816/items/b4f745e67588e8da7ed1
+
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract UpgradeableContractExample is Initializable {
